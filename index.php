26621aaba80612f96f324d883ca8530d8c3a8973
tomtoms <titiomm@gmail.com>
2013-11-12 00:05:36 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>	
+		<script src='getname.jquery.js' ></script>
+	</head>
+	<body>
+		First Name: <br />
+		<input type="text" name="fname" /><br />
+		Last name: <br />
+		<input type="text" name="lname" /><br /><br />
+		<input type="submit" name="submit" value="Send" />
+		<div id='response'>
+
+		</div>
+	</body>
+</html>
