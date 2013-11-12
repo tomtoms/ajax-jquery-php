e0f92c877c48c5ba47fcb3680bfe612d0e01b300
tomtoms <titiomm@gmail.com>
2013-11-12 09:52:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <html>
 	<head>
+		<title>Simple Ajax Call Demo</title>
 		<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>	
 		<script src='getname.jquery.js' ></script>
 	</head>
 	<body>
-		First Name: <br />
-		<input type="text" name="fname" /><br />
-		Last name: <br />
-		<input type="text" name="lname" /><br /><br />
-		<input type="submit" name="submit" value="Send" />
+	        <div id='form'>
+			First Name: <br />
+			<input type="text" name="fname" /><br />
+			Last name: <br />
+			<input type="text" name="lname" /><br /><br />
+			<input type="submit" name="submit" value="Send" />
+		</div>
 		<div id='response'>
 
 		</div>
