768997d7e6a810277f1a64cb14c2191d1c5bab4e
tomtoms <titiomm@gmail.com>
2013-11-12 14:48:18 +0100

    Update getname.ajax.php

--- getname.ajax.php
+++ getname.ajax.php
@@ -5,5 +5,9 @@ if(isset($_POST['fname']) && isset($_POST['lname'])){
     $string = '<p>First name: '.$fname."</p> <p>Last name: ".$lname." </p>";
     echo $string;    
 }
+else
+{
+    echo 'Error !';
+}
 
 ?>
